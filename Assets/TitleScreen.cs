ff0a00d16f9c30899a2aa8778cdf097c0ba0000d
Karl Zylinski <karl@zylinski.se>
2014-09-27 17:49:07 +0200

    GAMEJAM BUILD

--- Assets/TitleScreen.cs
+++ Assets/TitleScreen.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class TitleScreen : MonoBehaviour
 {
+	public void Start()
+	{
+		Screen.showCursor = false;
+	}
+
 	void Update()
 	{
 		if (Input.GetKeyDown("joystick button 0")
