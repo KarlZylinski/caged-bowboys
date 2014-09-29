a915455439a8a80027ebc5dfda2e0d21180c45f7
Karl Zylinski <karl@zylinski.se>
2014-09-29 21:25:10 +0200

    Player selection.

---
+++ Assets/PlayerSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public enum PlayerInputType
+{
+    None,
+    X360,
+    Ps4
+}
+
+public class PlayerSelector : MonoBehaviour
+{
+    public PlayerInputType[] InputTypes = new PlayerInputType[4];
+
+	void Start () {
+	    if (GameObject.Find("PlayerSelector") != gameObject)
+	    {
+	        Destroy(gameObject);
+	        return;
+	    }
+
+	    DontDestroyOnLoad(gameObject);
+	}
+}
--- Assets/TitleScreen.cs
+++ Assets/TitleScreen.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class TitleScreen : MonoBehaviour
 {
+    private PlayerSelector _player_selector;
+
 	public void Start()
 	{
+	    _player_selector = GameObject.Find("PlayerSelector").GetComponent<PlayerSelector>();
 		Screen.showCursor = false;
 	}
 
 	void Update()
 	{
-		if (Input.GetKeyDown("joystick button 0")
-			|| Input.GetKeyDown("joystick button 1")
-			|| Input.GetKeyDown("joystick button 2")
-			|| Input.GetKeyDown("joystick button 3"))
-		{
-			Application.LoadLevel(1);
-		}
+	    const string xbox360_controller_join_button = "button 2";
+	    const string ps4_controller_join_button = "button 1";
+	    const string xbox360_controller_leave_button = "button 1";
+	    const string ps4_controller_leave_button = "button 2";
+
+	    for (var i = 0; i < 4; ++i)
+	    {
+	        
+            if (Input.GetKeyDown("joystick " + (i + 1).ToString() + " " + xbox360_controller_leave_button) && _player_selector.InputTypes[i] == PlayerInputType.X360)
+	            _player_selector.InputTypes[i] = PlayerInputType.None;
+            else if (Input.GetKeyDown("joystick " + (i + 1).ToString() + " " + ps4_controller_leave_button) && _player_selector.InputTypes[i] == PlayerInputType.Ps4)
+	            _player_selector.InputTypes[i] = PlayerInputType.None;
+	        else if (Input.GetKeyDown("joystick " + (i + 1).ToString() + " " + xbox360_controller_join_button))
+	            _player_selector.InputTypes[i] = PlayerInputType.X360;
+	        else if (Input.GetKeyDown("joystick " + (i + 1).ToString() + " " + ps4_controller_join_button))
+	            _player_selector.InputTypes[i] = PlayerInputType.Ps4;
+	    }
 
 		if (Input.GetKeyDown(KeyCode.Escape))
 			Application.Quit();
